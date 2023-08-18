8827e72b7ac9e7171bfb53d6eeb2e9a7a82bb0a2
Ilya Breitburg <ilya.breytburg@gmail.com>
2023-08-18 13:44:29 +0300

    Add comments

--- lib/src/bulletin.dart
+++ lib/src/bulletin.dart
@@ -4,6 +4,17 @@ import 'package:flutter/material.dart';
 import 'package:screen_corners/screen_corners.dart';
 import 'package:smooth_corner/smooth_corner.dart';
 
+/// Shows a bulletin view (AirPods-like popup)
+///
+/// This function shows a bulletin view using [BulletinModalRoute].
+/// It is a wrapper around [Navigator.of(context).push].
+/// It is recommended to use this function instead of [Navigator.of(context).push].
+///
+/// [builder] is the builder for the bulletin view. It is required.
+/// [draggable] determines whether the bulletin view can be dragged up and down.
+/// [dismissible] determines whether the bulletin view can be dismissed by dragging it down or by tapping on the barrier.
+/// [height] is the height of the bulletin view. It defaults to 450.
+/// [backgroundColor] is the background color of the bulletin view. It defaults to the scaffold background color.
 Future<void> showBulletin({
   required BuildContext context,
   required WidgetBuilder builder,
@@ -26,6 +37,18 @@ Future<void> showBulletin({
   );
 }
 
+/// A modal route that shows a bulletin view (AirPods-like)
+///
+/// This route is used by [showBulletin] to show a bulletin view.
+///
+/// The route is built using [PageRouteBuilder] and uses a [SlideTransition]
+/// to animate the route in and out.
+///
+/// [height] is the height of the bulletin view. It defaults to 450.
+/// [draggable] determines whether the bulletin view can be dragged up and down.
+/// [dragDismissible] determines whether the bulletin view can be dismissed by dragging it down.
+/// [backgroundColor] is the background color of the bulletin view. It defaults to the scaffold background color.
+/// [builder] is the builder for the bulletin view. It is required.
 class BulletinModalRoute extends PageRouteBuilder {
   final double height;
   final bool draggable, dragDismissible;
@@ -74,6 +97,10 @@ class BulletinModalRoute extends PageRouteBuilder {
   }
 }
 
+/// A draggable bulletin view that is responsible for the drag animation
+/// 
+/// This widget is used by [BulletinModalRoute] to show a bulletin view.
+/// It is responsible for the drag animation and the dismiss animation.
 class _DraggableBulletinView extends StatefulWidget {
   final bool draggable, dragDismissible;
   final WidgetBuilder builder;
@@ -134,6 +161,10 @@ class _DraggableBulletinViewState extends State<_DraggableBulletinView>
     );
   }
 
+  /// Wraps the child with a [GestureDetector] to detect drag events
+  /// 
+  /// This widget is used by [_DraggableBulletinView] to detect drag events.
+  /// It is responsible for the drag animation.
   Widget _dragDetector({required Widget child}) {
     return GestureDetector(
       onVerticalDragUpdate: (details) {
